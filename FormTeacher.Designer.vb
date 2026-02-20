a02f3d4fd35fd2846d95c7be877f474e10ebbdcc
aaronlizaschool1-rgb <aaronlizaschool1@gmail.com>
2026-02-20 09:58:34 +0800

    a

---
+++ FormTeacher.Designer.vb
@@ -0,0 +1,343 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormTeacher
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Panel7 = New System.Windows.Forms.Panel()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Panel8 = New System.Windows.Forms.Panel()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Panel9 = New System.Windows.Forms.Panel()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        Me.Panel5.SuspendLayout()
+        Me.Panel6.SuspendLayout()
+        Me.Panel7.SuspendLayout()
+        Me.Panel8.SuspendLayout()
+        Me.Panel9.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.BackColor = System.Drawing.Color.Transparent
+        Me.TableLayoutPanel1.ColumnCount = 17
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 0.4349171!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.423923!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 0.9857928!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 2.628781!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 2.614379!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.53813!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.633987!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13.0719!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 2.081118!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.26765!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.198257!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13.83442!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.30719!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.803922!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 0.9803922!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.82952!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.133821!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel1, 4, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel2, 5, 5)
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel3, 7, 5)
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel4, 9, 5)
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel5, 11, 5)
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel6, 13, 5)
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel7, 15, 5)
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel8, 13, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel9, 13, 3)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 7
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.77778!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.77778!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.333333!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.55556!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.444445!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.111111!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.55556!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(918, 450)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.TableLayoutPanel1.SetColumnSpan(Me.Panel1, 8)
+        Me.Panel1.Controls.Add(Me.DataGridView1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Panel1.Location = New System.Drawing.Point(53, 119)
+        Me.Panel1.Name = "Panel1"
+        Me.TableLayoutPanel1.SetRowSpan(Me.Panel1, 3)
+        Me.Panel1.Size = New System.Drawing.Size(583, 213)
+        Me.Panel1.TabIndex = 0
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(583, 213)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(77, 358)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(155, 35)
+        Me.Panel2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button1.Font = New System.Drawing.Font("Franklin Gothic Heavy", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.DarkRed
+        Me.Button1.Location = New System.Drawing.Point(0, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(155, 35)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "CURRENT"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.Button2)
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel3.Location = New System.Drawing.Point(253, 358)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(114, 35)
+        Me.Panel3.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button2.Font = New System.Drawing.Font("Franklin Gothic Heavy", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.DarkRed
+        Me.Button2.Location = New System.Drawing.Point(0, 0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(114, 35)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "UNASSIGN"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Panel4
+        '
+        Me.Panel4.Controls.Add(Me.Button3)
+        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel4.Location = New System.Drawing.Point(392, 358)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(106, 35)
+        Me.Panel4.TabIndex = 3
+        '
+        'Button3
+        '
+        Me.Button3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button3.Font = New System.Drawing.Font("Franklin Gothic Heavy", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.DarkRed
+        Me.Button3.Location = New System.Drawing.Point(0, 0)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(106, 35)
+        Me.Button3.TabIndex = 0
+        Me.Button3.Text = "ASSIGN"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Panel5
+        '
+        Me.Panel5.Controls.Add(Me.Button4)
+        Me.Panel5.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel5.Location = New System.Drawing.Point(515, 358)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(121, 35)
+        Me.Panel5.TabIndex = 4
+        '
+        'Button4
+        '
+        Me.Button4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button4.Font = New System.Drawing.Font("Franklin Gothic Heavy", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.ForeColor = System.Drawing.Color.DarkRed
+        Me.Button4.Location = New System.Drawing.Point(0, 0)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(121, 35)
+        Me.Button4.TabIndex = 0
+        Me.Button4.Text = "NEW"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Panel6
+        '
+        Me.Panel6.Controls.Add(Me.Button5)
+        Me.Panel6.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel6.Location = New System.Drawing.Point(654, 358)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(84, 35)
+        Me.Panel6.TabIndex = 5
+        '
+        'Button5
+        '
+        Me.Button5.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button5.Font = New System.Drawing.Font("Franklin Gothic Heavy", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.ForeColor = System.Drawing.Color.DarkRed
+        Me.Button5.Location = New System.Drawing.Point(0, 0)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(84, 35)
+        Me.Button5.TabIndex = 0
+        Me.Button5.Text = "STATUS"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Panel7
+        '
+        Me.Panel7.Controls.Add(Me.Button6)
+        Me.Panel7.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel7.Location = New System.Drawing.Point(753, 358)
+        Me.Panel7.Name = "Panel7"
+        Me.Panel7.Size = New System.Drawing.Size(102, 35)
+        Me.Panel7.TabIndex = 6
+        '
+        'Button6
+        '
+        Me.Button6.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button6.Font = New System.Drawing.Font("Franklin Gothic Heavy", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.ForeColor = System.Drawing.Color.DarkRed
+        Me.Button6.Location = New System.Drawing.Point(0, 0)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(102, 35)
+        Me.Button6.TabIndex = 0
+        Me.Button6.Text = "DELETE"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Panel8
+        '
+        Me.TableLayoutPanel1.SetColumnSpan(Me.Panel8, 3)
+        Me.Panel8.Controls.Add(Me.ListBox1)
+        Me.Panel8.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel8.Location = New System.Drawing.Point(654, 119)
+        Me.Panel8.Name = "Panel8"
+        Me.Panel8.Size = New System.Drawing.Size(201, 92)
+        Me.Panel8.TabIndex = 7
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.Color.SeaShell
+        Me.ListBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListBox1.Font = New System.Drawing.Font("Franklin Gothic Medium", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.ForeColor = System.Drawing.Color.DarkRed
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 29
+        Me.ListBox1.Location = New System.Drawing.Point(0, 0)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(201, 92)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Panel9
+        '
+        Me.TableLayoutPanel1.SetColumnSpan(Me.Panel9, 3)
+        Me.Panel9.Controls.Add(Me.ListBox2)
+        Me.Panel9.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel9.Location = New System.Drawing.Point(654, 241)
+        Me.Panel9.Name = "Panel9"
+        Me.Panel9.Size = New System.Drawing.Size(201, 91)
+        Me.Panel9.TabIndex = 8
+        '
+        'ListBox2
+        '
+        Me.ListBox2.BackColor = System.Drawing.Color.SeaShell
+        Me.ListBox2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListBox2.Font = New System.Drawing.Font("Franklin Gothic Demi Cond", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox2.ForeColor = System.Drawing.Color.DarkRed
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 29
+        Me.ListBox2.Location = New System.Drawing.Point(0, 0)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(201, 91)
+        Me.ListBox2.TabIndex = 0
+        '
+        'FormTeacher
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.ACT3_MACAPAGAL_LUZADA.My.Resources.Resources.TEACHER_SCHEDULE
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(918, 450)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Name = "FormTeacher"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FormTeacher"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel6.ResumeLayout(False)
+        Me.Panel7.ResumeLayout(False)
+        Me.Panel8.ResumeLayout(False)
+        Me.Panel9.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents Panel7 As Panel
+    Friend WithEvents Panel8 As Panel
+    Friend WithEvents Panel9 As Panel
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents ListBox2 As ListBox
+End Class
---
+++ FormTeacher.vb
@@ -0,0 +1,219 @@
+﻿Imports System.ComponentModel
+Imports System.Runtime.InteropServices
+Imports ACT3_MACAPAGAL_LUZADA.FormTeacher
+Public Class FormTeacher
+
+    Const MAX_TEACHERS As Integer = 10
+
+    Dim teachers As New BindingList(Of Teacher_Structure)
+    Class Teacher_Structure
+        Public Property Name As String
+        Public Property Specialization As String
+        Public Property WorkingHours As Integer = 0
+        Public Property WorkingHours_Max As Integer = 7
+        Public Property AssignedClasses As New List(Of String)
+        Public Property IsOnLeave As Boolean = False
+
+        Public Sub New(name As String, specialization As String)
+            Me.Name = name
+            Me.Specialization = specialization
+        End Sub
+
+        Public Sub AssignClass(className As String, hours As Integer)
+            If IsOnLeave Then
+                MessageBox.Show($"{Name} is currently on leave.")
+                Exit Sub
+            End If
+
+            If WorkingHours + hours <= WorkingHours_Max Then
+                AssignedClasses.Add(className)
+                WorkingHours += hours
+            Else
+                MessageBox.Show($"Cannot assign {className} to {Name}. Exceeds maximum working hours.")
+            End If
+        End Sub
+    End Class
+
+
+    Private Sub RefreshDisplay()
+
+        Dim displayList = teachers.Select(Function(t) New With {
+            Key .Name = t.Name,
+            Key .Specialization = t.Specialization,
+            Key .WorkingHours_Max = t.WorkingHours_Max,
+            Key .WorkingHours = t.WorkingHours,
+            Key .Status = If(t.IsOnLeave, "On Leave", "Active"),
+            Key .AssignedClasses = String.Join(", ", t.AssignedClasses)
+        }).ToList()
+
+        DataGridView1.DataSource = Nothing
+        DataGridView1.DataSource = displayList
+
+
+        ListBox1.DataSource = Nothing
+        ListBox1.DataSource = teachers
+        ListBox1.DisplayMember = "Name"
+
+
+        For Each subject As String In teachers.SelectMany(Function(t) t.AssignedClasses).Distinct()
+            If Not ListBox2.Items.Contains(subject) Then
+                ListBox2.DataSource = Nothing
+
+                ListBox2.Items.Add(subject)
+            End If
+        Next
+
+
+
+    End Sub
+
+    ' Current Button +++++
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        teachers.Clear()
+        teachers.Add(New Teacher_Structure("Clara G. Bascon", "Mathematics"))
+        teachers.Add(New Teacher_Structure("Maria Elda V. Romero", "Science"))
+        teachers.Add(New Teacher_Structure("Helen L. Cayetano", "English"))
+        teachers.Add(New Teacher_Structure("Joy Marie T. Panaga", "Filipino"))
+        teachers.Add(New Teacher_Structure("Rene B. Ocson", "Social Studies"))
+        teachers.Add(New Teacher_Structure("Riza C. Dailo", "Physical Education"))
+        teachers.Add(New Teacher_Structure("Edna B. Arriola", "Music"))
+
+        RefreshDisplay()
+    End Sub
+
+    'assign class button +++++++
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        If ListBox1.SelectedIndex = -1 Then
+            MessageBox.Show("Select a teacher first.")
+            Exit Sub
+        End If
+
+        Dim subject As String = InputBox("Enter the subject to assign:", "Assign Class")
+        If String.IsNullOrWhiteSpace(subject) Then
+            MessageBox.Show("Subject cannot be empty.")
+            Exit Sub
+        End If
+
+        If subject.Length > 20 Then
+            MessageBox.Show("Subject name is too long. Please limit to 20 characters.")
+            Exit Sub
+        End If
+
+        If subject.Any(Function(c) Not Char.IsLetterOrDigit(c) AndAlso Not Char.IsWhiteSpace(c)) Then
+            MessageBox.Show("Subject name can only contain letters, numbers, and spaces.")
+            Exit Sub
+        End If
+
+        If teachers.Any(Function(t) t.AssignedClasses.Contains(subject)) Then
+            MessageBox.Show("This subject is already assigned to another teacher.")
+            Exit Sub
+        End If
+
+
+        Dim hoursInput = InputBox("Enter hours for the subject:", "Assign Class")
+        Dim hours As Integer
+        If Not Integer.TryParse(hoursInput, hours) Then
+            MessageBox.Show("Invalid input for hours.")
+            Exit Sub
+        End If
+
+        Dim selectedTeacher As Teacher_Structure = CType(ListBox1.SelectedItem, Teacher_Structure)
+        selectedTeacher.AssignClass(subject, hours)
+
+
+        RefreshDisplay()
+    End Sub
+
+
+
+
+    ' Status Button for Leave/Active +++++++
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If ListBox1.SelectedIndex = -1 Then
+            MessageBox.Show("Select a teacher first.")
+            Exit Sub
+        End If
+
+        Dim selectedTeacher As Teacher_Structure = CType(ListBox1.SelectedItem, Teacher_Structure)
+
+
+        selectedTeacher.IsOnLeave = Not selectedTeacher.IsOnLeave
+        MessageBox.Show($"{selectedTeacher.Name} is now {(If(selectedTeacher.IsOnLeave, "On Leave", "Active"))}.")
+        RefreshDisplay()
+    End Sub
+
+    ' Delete teacher button +++++++
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If ListBox1.SelectedIndex = -1 Then
+            MessageBox.Show("Select a teacher to delete.")
+            Exit Sub
+        End If
+
+        Dim selectedTeacher As Teacher_Structure = CType(ListBox1.SelectedItem, Teacher_Structure)
+        teachers.Remove(selectedTeacher)
+        MessageBox.Show("Teacher deleted successfully.")
+        RefreshDisplay()
+    End Sub
+    ' Remove subject from teacher button +++++++
+    Private Sub button2_click(sender As Object, e As EventArgs) Handles Button2.Click
+        If ListBox2.SelectedIndex = -1 Then
+
+            MessageBox.Show("Select a subject to remove.")
+            Exit Sub
+        End If
+
+        Dim selectedSubject As String = ListBox2.SelectedItem.ToString()
+        Dim hours As Integer
+        hours = InputBox("Enter hours to remove from the subject:", "Remove Class")
+        For Each teacher As Teacher_Structure In teachers
+            If hours = teacher.WorkingHours Then
+
+
+                If teacher.AssignedClasses.Contains(selectedSubject) Then
+                    teacher.AssignedClasses.Remove(selectedSubject)
+                    teacher.WorkingHours -= hours
+                End If
+
+            End If
+            If hours < teacher.WorkingHours Then
+                MessageBox.Show($"Cannot remove {hours} hours from {teacher.Name}. Not enough working hours assigned.")
+            End If
+        Next
+
+
+        MessageBox.Show("Subject removed from teachers")
+        ListBox2.Items.Remove(selectedSubject)
+        RefreshDisplay()
+
+    End Sub
+    'add new teacher button +++++++
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If teachers.Count >= MAX_TEACHERS Then
+            MessageBox.Show("Maximum number of teachers reached!")
+            Exit Sub
+        End If
+
+
+        Dim name = InputBox("Enter the new teacher's name:", "New Teacher")
+        If String.IsNullOrWhiteSpace(name) Then
+            MessageBox.Show("Name cannot be empty.")
+            Exit Sub
+        End If
+
+
+        Dim specialization = InputBox("Enter the new teacher's specialization:", "New Teacher")
+        If String.IsNullOrWhiteSpace(specialization) Then
+            MessageBox.Show("Specialization cannot be empty.")
+            Exit Sub
+        End If
+
+
+        teachers.Add(New Teacher_Structure(name, specialization))
+
+
+        RefreshDisplay()
+    End Sub
+
+
+End Class
